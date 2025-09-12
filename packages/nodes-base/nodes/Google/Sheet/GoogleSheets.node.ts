7ad39c98ede51bba716065b502bbbe4487f6f267
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-09-12 16:45:43 +0300

    feat: conflict

--- packages/nodes-base/nodes/Google/Sheet/GoogleSheets.node.ts
+++ packages/nodes-base/nodes/Google/Sheet/GoogleSheets.node.ts
@@ -13,7 +13,7 @@ export class GoogleSheets extends VersionedNodeType {
 			group: ['input', 'output'],
 			defaultVersion: 4.7,
 			subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-			description: 'Read, update and write data to Google Sheets',
+			description: 'Read, update and write data to Google Sheets (conflict)',
 		};
 
 		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
